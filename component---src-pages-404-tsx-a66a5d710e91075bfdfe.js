db6ef97692378a328ae2be18844774141e82c87e
Moritz Thüning <moritz.thuening@gmail.com>
2022-05-09 13:37:37 +0200

    Updates

---
+++ component---src-pages-404-tsx-a66a5d710e91075bfdfe.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcyberstudio=self.webpackChunkcyberstudio||[]).push([[218],{2224:function(e,t,n){n.r(t);var u=n(7294);t.default=function(){return u.createElement("main",null,u.createElement("title",null,"Not found"),u.createElement("h1",null,"Page not found"))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-a66a5d710e91075bfdfe.js.map
